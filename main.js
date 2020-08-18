5809d9a32c3ee06e5e39bd8907bea69e23807f63
MiMNets <mdmonirulislammonir@gmail.com>
2020-08-19 01:36:33 +0600

    show lyrics and get the single lyric

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
 document.getElementById('lyrics-button').addEventListener('click', function(){
     const lyricsInput = document.getElementById('lyrics-input').value;
-    let lyricsSearch = lyricsInput;
-    fetch(`https://api.lyrics.ovh/suggest/${lyricsSearch}`)
+    const lyricApi = `https://api.lyrics.ovh/suggest/${lyricsInput}`;
+    fetch(lyricApi)
     .then(res => res.json())
-    .then(lyricsData => {
+    .then(data => {
         for(let i = 0; i < 10; i++){
-            const element = lyricsData.data[i];
-            // console.log(element);
-            const lyricTitle = element.title;
-            const lyricArtist = element.artist.name;
-
+            const element = data.data[i];
             const lyricList = document.getElementById('lyrics-list');
-            lyricList.innerHTML = '';
-            lyricList.innerHTML += `<p class="author lead"><strong>${lyricTitle}</strong> - Album by - <span>${lyricArtist}</span> <button class="btn btn-success">Get Lyrics</button></p>`;
-            
-            fetch(`https://api.lyrics.ovh/v1/${lyricArtist}/${lyricTitle}`)
-            .then(res => res.json())
-            .then(lyrics => {                
-                return lyrics;         
-            })
+            lyricList.innerHTML += 
+            `
+            <div class="single-result row align-items-center my-3 p-3">
+                <div class="col-md-9">
+                    <h3 class="lyrics-name">${element.title}</h3>
+                    <p class="author lead">Album by <span>${element.artist.name}</span></p>
+                </div>
+                <div class="col-md-3 text-md-right text-center">
+                    <button onclick = "lyricsLink('${element.artist.name}','${element.title}')" class="btn btn-success">Get Lyrics</button>
+                </div>
+            `;
         }
     })
 })
+
+function lyricsLink(artist,title){
+    fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
+        .then(res => res.json())
+        .then(lyrics => {                
+        console.log(lyrics);
+        document.getElementById('lyrics-list').style.display = "none";
+        const lyricsView = document.getElementById('lyrics-view');
+            lyricsView.innerHTML += 
+            `
+                <button class="btn go-back">&lsaquo;</button>
+                <h2 class="text-success mb-4">${title} - ${artist}</h2>
+                <pre class="lyric text-white">${lyrics.lyrics}
+                </pre>
+            `;
+    })
+}
